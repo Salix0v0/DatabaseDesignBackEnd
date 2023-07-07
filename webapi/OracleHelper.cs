5b36d39683b40f2a88b0d39813219359627c796a
Salix0v0 <104568454+Salix0v0@users.noreply.github.com>
2023-07-07 16:04:12 +0800

    修改oraclehelper
    现在可从./appsettings.json自动读取connectionstring，简化了sql操作数据库的函数

--- webapi/OracleHelper.cs
+++ webapi/OracleHelper.cs
@@ -1,14 +1,55 @@
 ﻿using Oracle.ManagedDataAccess.Client;
 using System.Data;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+
 
 namespace webapi
 {
     public class OracleHelper
     {
-        public static string connectString = "Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=8.130.71.216)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=ORCLCDB)));User ID=system;password=123456";
+        public static string lastMessage="";
+        public static Boolean lastRe=false;
+        public static string _connectString = null;
+        public static string connectString { 
+            get
+            {
+                if(_connectString==null)
+                {
+                    String value = "";
+                    StreamReader sr = new StreamReader("./appsettings.json");
+                    value = sr.ReadToEnd();
+                    sr.Close();
+                    try
+                    {
+                        string[] strTmps = value.Split('"');
+                        for (int i = 0; i < strTmps.Length;i++)
+                        {
+                            if (strTmps[i].Equals("BloggingDatabase"))
+                            {
+                                return strTmps[i + 2];
+                            }
+                        }
+                        return "";
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                        throw;
+                    }
+                    return "";
+                }
+            else
+                    return _connectString;
+            }
+
+        }
+        
         #region 数据库连接
         public static OracleConnection DbConn(ref string message, ref Boolean re)
         {
+            
             //数据库的连接的方式
             OracleConnection conn;
             re = false;
@@ -31,6 +72,10 @@ namespace webapi
             }
 
             return conn;
+        }
+        public static OracleConnection DbConn()
+        {
+            return DbConn(ref lastMessage,ref lastRe);
         }
         #endregion
 
@@ -81,6 +126,10 @@ namespace webapi
                 message = "add数据出错,原因:" + ee.Message.ToString();
             }
             return re;
+        }
+        public static Boolean AddSql(string sql)
+        {
+            return AddSql(sql, ref lastMessage);
         }
         #endregion
 
@@ -182,6 +231,10 @@ namespace webapi
             }
             return re;
         }
+        public static Boolean UpdateSql(string sql)
+        {
+            return UpdateSql(sql,ref lastMessage);
+        }
         #endregion
 
         #region 查
@@ -236,7 +289,58 @@ namespace webapi
             }
             return dt;
         }
+        public static DataTable SelectSql(string sql)
+        {
+            return SelectSql(sql, ref lastMessage);
+        }
         #endregion
 
+        public Boolean SqlOption(string sql,ref string message)
+        {
+            bool re = false;
+            try
+            {
+                OracleConnection conn = DbConn(ref message, ref re);
+                if (conn == null)
+                {
+                    re = false;
+                    message = "数据库连接对象为空";
+                }
+                else
+                {
+                    try
+                    {
+                        if (conn.State == ConnectionState.Closed)
+                        {
+                            conn.Open();
+                        }
+                    }
+                    catch (Exception eee)
+                    {
+                        re = false;
+                        message = eee.Message.ToString();
+                    }
+                    OracleCommand cmd = new OracleCommand(sql, conn);
+                    int count = cmd.ExecuteNonQuery();
+                    if (count > 0)
+                    {
+                        re = true;
+                    }
+                    else
+                    {
+                        re = false;
+                        message = "数据更新失败";
+                    }
+                    cmd.Dispose();
+                    conn.Close();
+                }
+            }
+            catch (Exception ee)
+            {
+                re = false;
+                message = "add数据出错,原因:" + ee.Message.ToString();
+            }
+            return re;
+        }
     }
 }
